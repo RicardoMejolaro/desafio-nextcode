d2fb1654f262bd5cb4ea1f859bbfdd1595c44cc8
Ricardo Mejolaro <ricardo.mejolaro@gmail.com>
2021-07-19 00:31:19 -0300

    add one route get

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -5,6 +5,9 @@ import { PicturesController } from '../controllers/PicturesController';
 const router = express.Router();
 const picturesController = new PicturesController;
 
+router.get('/picture', (req, res) => {
+    res.status(200).send({ message: 'Welcome!'})
+})
 router.post('/picture', multer.array('files', 1), picturesController.create);
 
 export { router };
